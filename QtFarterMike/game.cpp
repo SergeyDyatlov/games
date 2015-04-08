c2e46f54af490b8a2a6afe7f74cb2c6a1670a2b3
Sergey Dyatlov <application.processmessages@gmail.com>
2015-04-08 12:55:43 +0600

    Пришлось заменить SDL_RENDERER_SOFTWARE на SDL_RENDERER_ACCELERATED, иначе текстуры пропадали при зеркалировании (SDL_FLIP_*)

--- QtFarterMike/game.cpp
+++ QtFarterMike/game.cpp
@@ -15,7 +15,7 @@ void Game::Init(const char* Caption, int Width, int Height)
                 Width,
                 Height,
                 SDL_WINDOW_SHOWN);
-    FRenderer = SDL_CreateRenderer(FWindow, -1, SDL_RENDERER_SOFTWARE);
+    FRenderer = SDL_CreateRenderer(FWindow, -1, SDL_RENDERER_ACCELERATED);
 
     Font.LoadFromFile(FRenderer, "res/font.png");
 
